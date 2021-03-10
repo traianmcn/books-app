f858d6ae7023e1b9edd9e776ea8071f251019560
Traian Mocanu <traianmcn@gmail.com>
2021-03-10 20:29:03 +0200

    first commit

---
+++ src/main/java/com/booksapp/booksapp/BooksAppApplication.java
@@ -0,0 +1,15 @@
+package com.booksapp.booksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAppApplication.class, args);
+		System.out.println("adasd");
+	}
+
+
+}
---
+++ src/test/java/com/booksapp/booksapp/BooksAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booksapp.booksapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
