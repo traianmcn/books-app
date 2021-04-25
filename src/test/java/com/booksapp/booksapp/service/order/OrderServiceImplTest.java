272adb54ea7b645df0e875b894cb852e3f975601
Traian Mocanu <traianmcn@gmail.com>
2021-04-25 20:39:22 +0300

    small refactor

---
+++ src/test/java/com/booksapp/booksapp/service/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.booksapp.booksapp.service.order;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void getOrdersByCustomerId() {
+    }
+
+    @Test
+    void getOrderById() {
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+}
